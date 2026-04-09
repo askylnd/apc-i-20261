29943472b754264305f676aaba415d2f90505426
wendel <vianalupin@gmail.com>
2026-04-08 21:05:16 -0300

    não  terminei as praticas 03

---
+++ praticas/pratica03/declara_variaveis.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int idade;
+    char sexo;
+    float altura;
+    float peso;
+
+    idade = 19;
+    sexo = 'M';
+    altura = 1.80;
+    peso = 51.00;
+
+    printf("idade: %i\n", idade);     
+
+    return 0;
+
+
+}
