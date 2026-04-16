286c043794eb01da7b4fc360eedfde08aeb5db22
wendel <vianalupin@gmail.com>
2026-04-15 21:36:01 -0300

    codigos da aula05

---
+++ aulas/aula05/operadores_relacionais.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+ int idade;
+
+printf("entre com a sua idade: ");
+scanf("%i",&idade);
+while(getchar() != '\n');
+
+// operadores relacionais
+// <= menor que 
+// <= menor ou igual
+// > maior que
+// >= maior ou igual
+// == igualdade
+// ! = diferenca
+
+int eh_menor_idade = idade < 18;
+int eh_idoso = idade >-  60;
+int eh_debutante = idade ==15;
+int nao_eh_debutante = idade != 15;
+
+printf("voce eh menor de idadae? %i\n", eh_menor_idade);
+printf("voce eh idoso/ %i\n", eh_idoso);
+printf("voce eh debutante? %i\n", eh_debutante);
+printf("voce nao eh debutante/ %i\n", nao_eh_debutante);
+
+return 0;
+
+}
+
+   
